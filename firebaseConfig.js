fc13a46e2f5adc868f7265a034c50c19572e468d
Khomsan <48270055+bird348@users.noreply.github.com>
2020-12-02 14:20:11 +0700

    delete firebaseConfig

--- firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const FIREBASE_CONFIG = {
-        apiKey: "AIzaSyDT9Lepqg3UoJRBiE0NFCMA2qYk8TZa4MQ",
-        authDomain: "sampleblog-by-b.firebaseapp.com",
-        databaseURL: "https://sampleblog-by-b.firebaseio.com",
-        projectId: "sampleblog-by-b",
-        storageBucket: "sampleblog-by-b.appspot.com",
-        messagingSenderId: "4419566716",
-        appId: "1:4419566716:web:54d833dc160199102d9df9",
-    };
